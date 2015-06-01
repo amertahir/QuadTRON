234ec39052baa23a33409b87aecd72a0c1f1a42d
Amer Tahir <amertahir@gmail.com>
2015-05-31 23:19:33 -0400

    pre-submission changes

--- GroundControlStation/src/edu/umbc/cmsc691/updave/MainWindow.java
+++ GroundControlStation/src/edu/umbc/cmsc691/updave/MainWindow.java
@@ -4,10 +4,15 @@ import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
+import java.util.Base64;
+import java.util.TimerTask;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -22,6 +27,7 @@ import com.digi.xbee.api.utils.ByteUtils;
 import com.esri.runtime.ArcGISRuntime;
 import com.esri.toolkit.overlays.NavigatorOverlay;
 import com.esri.toolkit.overlays.ScaleBarOverlay;
+import com.esri.core.geometry.Envelope;
 import com.esri.core.geometry.GeometryEngine;
 import com.esri.core.geometry.Point;
 import com.esri.core.geometry.SpatialReference;
@@ -54,22 +60,29 @@ import javax.swing.JButton;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.geom.AffineTransform;
 
 import javax.swing.JRadioButton;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
+
+import java.util.Timer;
+import javax.swing.SwingConstants;
+import java.awt.Font;
 
 public class MainWindow implements IDataReceiveListener {
 
 	// XBee stuff
 	/* Constants */
     // Port where the XBee module is connected to.
-    private static final String PORT = "COM6";
+    private static final String PORT = "COM12";
     // Baud rate of the XBee module.
     private static final int BAUD_RATE = 115200;
     // XBee MACs
-    public static String baseStationMAC = "0013A200408BA39A";
-    public static String uav1MAC = "0013A20040A5442D";
+    //public static String baseStationMAC = "0013A200408BA39A";
+    //public static String uav1MAC = "0013A20040A5442D";
+    public static String baseStationMAC = "0013A20040C8D2F2";
+    public static String uav1MAC = "0013A20040E83D64";
     public static String broadcastMAC = XBee64BitAddress.BROADCAST_ADDRESS.toString();
     // XBee MTU
     public static final int xbeeMTU = 100;
@@ -93,6 +106,17 @@ public class MainWindow implements IDataReceiveListener {
 	private static SpatialReference wgs84 = SpatialReference.create(4326);
 	private JLabel imgFrame;
 	public String thermalImagePalette = "grayscale";
+	public boolean mapRecentered = false;
+	private JLabel lblHeartbeatsValue;
+	private JLabel lblAirspeedValue;
+	private JLabel lblGroundSpeedValue;
+	private JLabel lblArmedValue;
+	private JLabel lblDetectedValue;
+	private JLabel lblAltitudeValue;
+	/**
+	 * @wbp.nonvisual location=50,80
+	 */
+	private final Timer timer = new Timer();
 
 	/**
 	 * Launch the application.
@@ -135,8 +159,6 @@ public class MainWindow implements IDataReceiveListener {
             xbeeRadio.open();
             xbeeRadio.addDataListener(this);
             System.out.println("Radio initialized");
-            // send alive broadcast that includes system date-time for UAVs to sync their date-time with
-            sendGCSMessage(broadcastMAC, new String("SYS_TIME " + (System.currentTimeMillis() / 1000L)));
         } catch (XBeeException e) {
             System.out.println("Can't connect to radio");
             e.printStackTrace();
@@ -209,47 +231,116 @@ public class MainWindow implements IDataReceiveListener {
 	    panel.add(imgFrame);
 	    
 	    JButton btnThermalCameraFfc = new JButton("Thermal Camera FFC");
-	    btnThermalCameraFfc.setBounds(10, 250, 180, 25);
+	    btnThermalCameraFfc.setBounds(10, 595, 180, 25);
 	    btnThermalCameraFfc.addActionListener(new ActionListener() {
 	    	public void actionPerformed(ActionEvent e) {
-	    		sendGCSMessage(uav1MAC, "TCAM_FFC");
+	    		GCSSendMessage(uav1MAC, "TCAM_FFC");
 	    	}
 	    });
 	    panel.add(btnThermalCameraFfc);
 	    
 	    JButton btnStopMessaging = new JButton("Stop Messaging");
-	    btnStopMessaging.setBounds(10, 300, 180, 25);
+	    btnStopMessaging.setBounds(10, 645, 180, 25);
 	    btnStopMessaging.addActionListener(new ActionListener() {
 	    	public void actionPerformed(ActionEvent e) {
-	    		sendGCSMessage(uav1MAC, "STOP");
+	    		GCSSendMessage(uav1MAC, "STOP");
 	    	}
 	    });
 	    panel.add(btnStopMessaging);
 	    
 	    JRadioButton rdbtnColor = new JRadioButton("Color");
-	    rdbtnColor.setBounds(10, 162, 80, 23);
+	    rdbtnColor.setBounds(10, 5, 80, 23);
 	    rdbtnColor.setActionCommand("color");
 	    rdbtnColor.addActionListener(new ActionListener() {
 	    	public void actionPerformed(ActionEvent e) {
 	    		thermalImagePalette = e.getActionCommand();
 	    	}
 	    });
+	    rdbtnColor.setVisible(false);
 	    panel.add(rdbtnColor);
 	    
 	    JRadioButton rdbtnGrayscale = new JRadioButton("Grayscale");
 	    rdbtnGrayscale.setSelected(true);
-	    rdbtnGrayscale.setBounds(102, 162, 98, 23);
+	    rdbtnGrayscale.setBounds(102, 5, 98, 23);
 	    rdbtnGrayscale.setActionCommand("grayscale");
 	    rdbtnGrayscale.addActionListener(new ActionListener() {
 	    	public void actionPerformed(ActionEvent e) {
 	    		thermalImagePalette = e.getActionCommand();
 	    	}
 	    });
+	    rdbtnGrayscale.setVisible(false);
 	    panel.add(rdbtnGrayscale);
 	    
 	    ButtonGroup thermalImagePaletteButtonGroup = new ButtonGroup();
 	    thermalImagePaletteButtonGroup.add(rdbtnColor);
 	    thermalImagePaletteButtonGroup.add(rdbtnGrayscale);
+	    
+	    JLabel lblAirspeed = new JLabel("Airspeed");
+	    lblAirspeed.setHorizontalAlignment(SwingConstants.CENTER);
+	    lblAirspeed.setBounds(60, 240, 80, 14);
+	    panel.add(lblAirspeed);
+	    
+	    JLabel lblGroundspeed = new JLabel("Groundspeed");
+	    lblGroundspeed.setHorizontalAlignment(SwingConstants.CENTER);
+	    lblGroundspeed.setBounds(60, 320, 80, 14);
+	    panel.add(lblGroundspeed);
+	    
+	    JLabel lblHeartbeats = new JLabel("Heartbeats");
+	    lblHeartbeats.setHorizontalAlignment(SwingConstants.CENTER);
+	    lblHeartbeats.setBounds(60, 170, 80, 14);
+	    panel.add(lblHeartbeats);
+	    
+	    JLabel lblAltitude = new JLabel("Altitude");
+	    lblAltitude.setHorizontalAlignment(SwingConstants.CENTER);
+	    lblAltitude.setBounds(60, 400, 80, 14);
+	    panel.add(lblAltitude);
+	    
+	    JLabel lblUavArmed = new JLabel("UAV Armed");
+	    lblUavArmed.setHorizontalAlignment(SwingConstants.CENTER);
+	    lblUavArmed.setBounds(60, 480, 80, 14);
+	    panel.add(lblUavArmed);
+	    
+	    lblHeartbeatsValue = new JLabel("--");
+	    lblHeartbeatsValue.setFont(new Font("Tahoma", Font.BOLD, 22));
+	    lblHeartbeatsValue.setForeground(new Color(25, 25, 112));
+	    lblHeartbeatsValue.setHorizontalAlignment(SwingConstants.CENTER);
+	    lblHeartbeatsValue.setBounds(20, 190, 160, 30);
+	    panel.add(lblHeartbeatsValue);
+	    
+	    lblAirspeedValue = new JLabel("--");
+	    lblAirspeedValue.setHorizontalAlignment(SwingConstants.CENTER);
+	    lblAirspeedValue.setForeground(new Color(128, 0, 128));
+	    lblAirspeedValue.setFont(new Font("Tahoma", Font.BOLD, 22));
+	    lblAirspeedValue.setBounds(20, 260, 160, 30);
+	    panel.add(lblAirspeedValue);
+	    
+	    lblGroundSpeedValue = new JLabel("--");
+	    lblGroundSpeedValue.setHorizontalAlignment(SwingConstants.CENTER);
+	    lblGroundSpeedValue.setForeground(new Color(128, 0, 128));
+	    lblGroundSpeedValue.setFont(new Font("Tahoma", Font.BOLD, 22));
+	    lblGroundSpeedValue.setBounds(20, 340, 160, 30);
+	    panel.add(lblGroundSpeedValue);
+	    
+	    lblAltitudeValue = new JLabel("--");
+	    lblAltitudeValue.setHorizontalAlignment(SwingConstants.CENTER);
+	    lblAltitudeValue.setForeground(new Color(0, 128, 0));
+	    lblAltitudeValue.setFont(new Font("Tahoma", Font.BOLD, 22));
+	    lblAltitudeValue.setBounds(20, 420, 160, 30);
+	    panel.add(lblAltitudeValue);
+	    
+	    lblArmedValue = new JLabel("--");
+	    lblArmedValue.setHorizontalAlignment(SwingConstants.CENTER);
+	    lblArmedValue.setForeground(new Color(139, 0, 0));
+	    lblArmedValue.setFont(new Font("Tahoma", Font.BOLD, 22));
+	    lblArmedValue.setBounds(20, 500, 160, 30);
+	    panel.add(lblArmedValue);
+	    
+	    lblDetectedValue = new JLabel("");
+	    lblDetectedValue.setFont(new Font("Tahoma", Font.BOLD, 18));
+	    lblDetectedValue.setForeground(new Color(255, 0, 0));
+	    lblDetectedValue.setHorizontalAlignment(SwingConstants.CENTER);
+	    lblDetectedValue.setBounds(0, 540, 200, 30);
+	    panel.add(lblDetectedValue);
 		
 		// cleanup just before application window is closed.
 	    frmGroundControlStation.addWindowListener(new WindowAdapter() {
@@ -266,14 +357,29 @@ public class MainWindow implements IDataReceiveListener {
 	        }
 	      }
 	    });
+	    
+	    // Timer for recurring stuff
+	    timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				// send alive broadcast that includes system date-time for UAVs to sync their date-time with
+	            GCSBroadcastSysTime();
+			}
+	    }, 2000, 10000);
 	}
 	
-	private void sendGCSMessage(String destMAC, String msg) {
+	private void GCSBroadcastSysTime() {
+		GCSSendMessage(broadcastMAC, new String("SYS_TIME " + System.currentTimeMillis()));
+	}
+	
+	private void GCSSendMessage(String destMAC, String msg) {
 		String gcsMessage = "GCS > " + msg; 
 		try {
 			System.out.println(gcsMessage);
 			System.out.println();
-			xbeeRadio.sendDataAsync(new XBee64BitAddress(destMAC), gcsMessage.getBytes());
+			synchronized(this) {
+				xbeeRadio.sendDataAsync(new XBee64BitAddress(destMAC), gcsMessage.getBytes());
+			}
 		} catch (XBeeException e) {
 			e.printStackTrace();
 		}
@@ -320,10 +426,42 @@ public class MainWindow implements IDataReceiveListener {
         try {
         	JSONObject jsonMsg = new JSONObject(message);
         	System.out.println("heartbeats: " + jsonMsg.getInt("heartbeats") + ", lastRSSI: " + jsonMsg.getInt("lastRSSI"));
-        	JSONObject jsonThermalFrame = jsonMsg.getJSONObject("thermalFrame");
-        	System.out.println("frameNumber: " + jsonThermalFrame.getInt("frameNumber") + ", FPA Temperature: " + (jsonThermalFrame.getDouble("fpaTemp") - 273.15) + ", Housing Temperature: " + (jsonThermalFrame.getDouble("housingTemp") - 273.15));
-        	// create thermal cam image from received data
-        	updateThermalImage(jsonThermalFrame.getString("data"));
+        	System.out.println("lat: " + jsonMsg.getDouble("lat") + ", lon: " + jsonMsg.getDouble("lon") + ", alt: " + jsonMsg.getDouble("alt") + ", heading: " + jsonMsg.getInt("heading") + ", airspeed: " + jsonMsg.getDouble("airspeed") + ", groundspeed: " + jsonMsg.getDouble("groundspeed") + ", armed: " + jsonMsg.getBoolean("armed"));
+        	if (jsonMsg.has("thermalImage")) {
+        		// create thermal cam image from received data
+            	updateThermalImage(jsonMsg.getString("thermalImage"));
+        	}
+        	
+        	// Re-center map if not done already
+            if (!this.mapRecentered) {
+            	this.mapRecentered = true;
+            	map.zoomTo(jsonMsg.getDouble("lat"), jsonMsg.getDouble("lon"), 18);
+            }
+            
+            // change drone position and heading
+            ((PictureMarkerSymbol)uavSymbol).setAngle(jsonMsg.getInt("heading"));
+            //int uavNewSize = Math.round((float)((jsonMsg.getDouble("alt")/40.0) * 60.0)) + 16;
+            //if (uavNewSize > 76) uavNewSize = 76;
+            //((PictureMarkerSymbol)uavSymbol).setSize(uavNewSize, uavNewSize);
+            Point uavNewPos = (Point)GeometryEngine.project(new Point(jsonMsg.getDouble("lon"), jsonMsg.getDouble("lat")), wgs84, map.getSpatialReference());
+            graphicsLayer.movePointGraphic(uav1SymbolId, uavNewPos);
+            graphicsLayer.updateGraphic(uav1SymbolId, uavSymbol);
+            
+            // update UAV info
+            lblHeartbeatsValue.setText(jsonMsg.getInt("heartbeats") + "");
+            lblAirspeedValue.setText(jsonMsg.getDouble("airspeed") + " mph");
+            lblGroundSpeedValue.setText(jsonMsg.getDouble("groundspeed") + " mph");
+            lblAltitudeValue.setText(jsonMsg.getDouble("alt") + " m");
+            if (jsonMsg.getBoolean("armed")) {
+            	lblArmedValue.setText("Armed");
+            } else {
+            	lblArmedValue.setText("Disarmed");
+            }
+            if (jsonMsg.has("detection")) {
+            	lblDetectedValue.setText("PEOPLE DETECTED");
+            } else {
+            	lblDetectedValue.setText("");
+            }
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
@@ -331,8 +469,20 @@ public class MainWindow implements IDataReceiveListener {
 	}
 	
 	private void updateThermalImage(String encodedThermalImageData) {
-		BufferedImage thermalImage = Util.generateThermalImage(encodedThermalImageData, thermalImagePalette);
-		imgFrame.setIcon(new ImageIcon(thermalImage));
+		//BufferedImage thermalImage = Util.generateThermalImage(encodedThermalImageData, thermalImagePalette);
+		byte[] bs = Base64.getDecoder().decode(encodedThermalImageData);
+		InputStream in = new ByteArrayInputStream(bs);
+		BufferedImage thermalImage;
+		try {
+			thermalImage = ImageIO.read(in);
+			AffineTransform tx = new AffineTransform();
+			tx.scale(2.5, 2.5);
+			AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_BICUBIC);
+			thermalImage = op.filter(thermalImage, null);
+			imgFrame.setIcon(new ImageIcon(thermalImage));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	private Symbol makeUAVSymbol() {
